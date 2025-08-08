a37138496a60096a4e30f730084e515063c43c0b
eunsu <156741951+eunsu0325@users.noreply.github.com>
2025-08-08 13:17:56 +0900

    Update train_scr.py

--- scr/train_scr.py
+++ scr/train_scr.py
@@ -18,6 +18,8 @@ import torch
 from torch.utils.data import DataLoader, Subset
 
 # Project imports
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from config import ConfigParser
 
 from models import ccnet, MyDataset, get_scr_transforms
